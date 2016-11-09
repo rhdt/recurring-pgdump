fc186081ce764e3738acd4ac1815065697bc3cac
Syam G Krishnan <sgk@riseup.net>
2016-11-09 19:39:05 +0530

    add dumpit script

---
+++ dumpit.sh
@@ -0,0 +1,14 @@
+while true
+do
+#extracting hour from date
+TIMEHR=$(date +%H)
+if [ $TIMEHR -eq $BKPTIME ];
+then
+echo "start backup"
+date
+pg_dump -h $PGHOST -p $PGPORT -U $PGUSER -d $PGDB -Ft > /var/lib/postgresql/data/`date --utc +%FT%T`.tar 
+date
+echo "end backup" 
+sleep 24h
+fi
+done
